85527ce405cf0283bb1a89302a7ff6b5c60b4ca5
Martin Marsicano <martinmarsicano@MMMBP16.local>
2026-02-09 18:37:32 -0300

    Percona Everest (Open Everest) ui-extension that adds Rancher Top-Level menu and sub-menus for Everest objects

---
+++ pkg/everest/routing/extension-routing.js
@@ -0,0 +1,47 @@
+import ListResource from '@shell/pages/c/_cluster/_product/_resource/index.vue';
+import CreateResource from '@shell/pages/c/_cluster/_product/_resource/create.vue';
+import ViewResource from '@shell/pages/c/_cluster/_product/_resource/_id.vue';
+import ViewNamespacedResource from '@shell/pages/c/_cluster/_product/_resource/_namespace/_id.vue';
+
+const PRODUCT = 'everest';
+
+const routes = [
+  {
+    name:      `${ PRODUCT }-c-cluster-resource`,
+    path:      `/:cluster/${ PRODUCT }/:resource`,
+    component: ListResource,
+    meta:      {
+      product: PRODUCT,
+      pkg:     PRODUCT
+    },
+  },
+  {
+    name:      `${ PRODUCT }-c-cluster-resource-create`,
+    path:      `/:cluster/${ PRODUCT }/:resource/create`,
+    component: CreateResource,
+    meta:      {
+      product: PRODUCT,
+      pkg:     PRODUCT
+    },
+  },
+  {
+    name:      `${ PRODUCT }-c-cluster-resource-id`,
+    path:      `/:cluster/${ PRODUCT }/:resource/:id`,
+    component: ViewResource,
+    meta:      {
+      product: PRODUCT,
+      pkg:     PRODUCT
+    },
+  },
+  {
+    name:      `${ PRODUCT }-c-cluster-resource-namespace-id`,
+    path:      `/:cluster/${ PRODUCT }/:resource/:namespace/:id`,
+    component: ViewNamespacedResource,
+    meta:      {
+      product: PRODUCT,
+      pkg:     PRODUCT
+    },
+  },
+];
+
+export default routes;
